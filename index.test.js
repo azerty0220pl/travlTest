4692985e7c98faa214dd3c5821c3de0a316181e0
Szymon Kokot <azerty0220pl@gmail.com>
2023-07-10 16:27:41 +0200

    Room.bookings tests

--- index.test.js
+++ index.test.js
@@ -42,3 +42,68 @@ describe("Room.name.length is greater than 0", () => {
         expect(new Room("", [], 1, 0)).toThrow();
     });
 });
+
+// Room.bookings                /////////////////////////////////////////////////////////////////////////////////////////
+describe("Room.bookings is array", () => {
+    it("is empty array", () => {
+        expect(Array.isArray(new Room("a", [], 1, 0).bookings)).toBeTruthy();
+    });
+
+    it("is array (not empty)", () => {
+        expect(Array.isArray(new Room("a", [0, 1, 2], 1, 0).bookings)).toBeTruthy();
+    });
+
+    it("is boolean", () => {
+        expect(new Room("a", false, 1, 0)).toThrow();
+    });
+
+    it("is int", () => {
+        expect(new Room("a", 1, 1, 0)).toThrow();
+    });
+
+    it("is string", () => {
+        expect(new Room("a", "Some String", 1, 0)).toThrow();
+    });
+});
+
+describe("Room.bookings elements to be Booking objects", () => {
+    it("is empty", () => {
+        new Room("", [], 0, 0).bookings.forEach(el => {
+            expect(el instanceof Booking).toBeTruthy();
+        });
+    });
+
+    it("is array of proper Booking", () => {
+        new Room("a", [new Booking("name", "email", new Date(), new Date(), 41, new Room("A - 303", [], 14500, 40))], 1, 0).bookings.forEach(el => {
+            expect(el instanceof Booking).toBeTruthy();
+        });
+    });
+
+    it("is array of proper Booking", () => {
+        new Room("a", [new Booking("name", "email", new Date(), new Date(), 41, new Room("A - 303", [], 14500, 40)), new Booking("name", "email", new Date(), new Date(), 41, new Room("A - 303", [], 14500, 40))], 1, 0)
+            .bookings.forEach(el => {
+                expect(el instanceof Booking).toBeTruthy();
+            });
+    });
+
+    it("is array of int", () => {
+        new Room("a", [0, 1, 2, 3, 4, 5], 1, 0)
+            .bookings.forEach(el => {
+                expect(el instanceof Booking).toBeTruthy();
+            });
+    });
+
+    it("is array of mixed types", () => {
+        new Room("a", [new Booking("name", "email", new Date(), new Date(), 41, new Room("A - 303", [], 14500, 40)), 1, false, 3, 4, 5], 1, 0)
+            .bookings.forEach(el => {
+                expect(el instanceof Booking).toBeTruthy();
+            });
+    });
+
+    it("is array of mixed types", () => {
+        new Room("a", [null, 1.1, true, 3, 4, 5], 1, 0)
+            .bookings.forEach(el => {
+                expect(el instanceof Booking).toBeTruthy();
+            });
+    });
+});
