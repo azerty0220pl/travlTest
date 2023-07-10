4ef36aaf4aef4b2b1f41ef03425c3222eb749e5f
Szymon Kokot <azerty0220pl@gmail.com>
2023-07-10 16:26:48 +0200

    Classes with empty classes created

---
+++ index.js
@@ -0,0 +1,44 @@
+class Room {
+    constructor(name, bookings, rate, discount) {
+        this.name = name;
+        this.bookings = bookings;
+        this.rate = rate;
+        this.discount = discount;
+    }
+
+    isOccupied(date) {
+        return;
+    }
+
+    occupancyPercetage(startDate, endDate) {
+        return;
+    }
+
+    static totalOccupancyPercentage(rooms, startDate, endDate) {
+        return;
+    }
+
+    static availableRooms(rooms, startDate, endDate) {
+        return;
+    }
+}
+
+class Booking {
+    constructor (name, email, checkIn, checkOut, discount, room) {
+        this.name = name;
+        this.email = email;
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+        this.discount = discount;
+        this.room = room;
+    }
+
+    getFee(){
+        return;
+    }
+}
+
+module.exports = {
+    Room: Room,
+    Booking: Booking
+}
